393ca9906c3ebbf5896d9aa93cdcef7710d0cb3c
David <david.rumpf@stud.uni-due.de>
2024-07-16 07:07:20 +0200

    .

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
-import os
-import pandas as pd
-import numpy as np
-import unicodedata
+from langchain_text_splitters import  RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_community.vectorstores import FAISS
-from transformers import AutoTokenizer, AutoModel
-import torch
-import re
+from get_embedding_function import get_embedding_function
+
+from langchain_core.documents import Document
 
 
-data_path = "paper/[15] Proactive vehicle routing with inferred demand to solve the bikesharing rebalancing problem.pdf"
 
-def load_documents():
+data_path = "paper/[1] Probabilistic Forecasts of Bike-Sharing Systems for Journey Planning.pdf"
+
+def load_documents(FILE_PATH):
     loader = PyPDFLoader(data_path)
     documents = loader.load()
     return documents
 
-print(load_documents())
+
+def split_documents(documents: list[Document]):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=800,
+        chunk_overlap=80,
+        length_function=len,
+        is_separator_regex=False
+    )
+    return text_splitter.split_documents(documents)
+
+
+
+document = load_documents(data_path)
+
+splitted_documents = split_documents(document)
+print(splitted_documents[1])
